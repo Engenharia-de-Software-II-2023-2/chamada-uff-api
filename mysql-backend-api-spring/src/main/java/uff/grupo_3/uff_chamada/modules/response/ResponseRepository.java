a1c9f44282a371934985ebc27ea7ff36a96f191e
Matheus Folly <matheusfolly@id.uff.br>
2023-11-02 12:13:24 -0300

    adicionado endpoint para consulta de alunos que responderam uma chamada (attendance) e endpoint para professor validar/invalidar a presença de um aluno

--- mysql-backend-api-spring/src/main/java/uff/grupo_3/uff_chamada/modules/response/ResponseRepository.java
+++ mysql-backend-api-spring/src/main/java/uff/grupo_3/uff_chamada/modules/response/ResponseRepository.java
@@ -1,9 +1,13 @@
 package uff.grupo_3.uff_chamada.modules.response;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ResponseRepository extends JpaRepository<Response, Integer> {
+
+    List<Response> findAllByAttendanceId(int attendanceId);
     
 }
